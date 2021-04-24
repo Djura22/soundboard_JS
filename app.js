97879b800ebd4d8bc5f1752261d70fff2f42c63b
Djura22 <michaeliangroves@gmail.com>
2021-04-24 18:37:17 +0100

    Stop Button functional

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// const PORT = process.env.PORT
-
 const soundsElement = document.querySelector('#sounds');
+const stopButton = document.querySelector('#stopButton');
 
 (async () => {
   const sounds = await getSounds();
@@ -14,16 +13,23 @@ async function getSounds() {
 }
 
 function addSoundsToPage(sounds) {
+
+  const players = []
+
   sounds.forEach(sound => {
     const soundDiv = document.createElement('div');
     soundDiv.className = 'sound';
+
     const soundTitle = document.createElement('h2');
     soundTitle.textContent = sound.title;
+
     soundDiv.appendChild(soundTitle);
 
     const player = document.createElement('audio');
     player.setAttribute('src', `sounds/${sound.src}`)
     soundDiv.appendChild(player);
+    players.push({ player, soundDiv });
+
 
     soundDiv.addEventListener('mousedown', () => {
       soundDiv.style.background = "#B22222"
@@ -31,14 +37,55 @@ function addSoundsToPage(sounds) {
       player.play();
     });
 
+
     soundDiv.addEventListener('mouseup', () => {
       soundDiv.style.background = ""
     });
 
     soundsElement.appendChild(soundDiv);
   });
+
+  document.querySelector('#stopButton').addEventListener('click', stopAll);
+
+  const keyCodes = {
+    65: 0, 
+    83: 1, 
+    68: 2, 
+    70 :3, 
+    71 :4, 
+    72: 5, 
+    74: 6, 
+    76: 7, 
+    186: 8, 
+    222: 9, 
+    90: 10,
+    75: 11
+  }
+
+  document.addEventListener('keydown', (event) => {
+    const playerIndex = keyCodes[event.keyCode];
+    const playerAndDiv = players[playerIndex];
+    if (playerAndDiv) {
+      playerAndDiv.soundDiv.style.background = "#B22222"
+      playerAndDiv.player.currentTime = 0;
+      playerAndDiv.player.play();
+    }
+
+  });
+
+  document.addEventListener('keyup', (event) => {
+    const playerIndex = keyCodes[event.keyCode];
+    const playerAndDiv = players[playerIndex];
+    if (playerAndDiv) {
+      playerAndDiv.soundDiv.style.background = ""
+    }
+
+  });
+
+  function stopAll() {
+    players.forEach(({player}) => {
+      player.pause();
+    });
+  }
 }
 
-server.listen(PORT, function() {
-  console.log("Server is running");
-})
